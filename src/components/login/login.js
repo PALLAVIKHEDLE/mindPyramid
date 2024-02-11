00b9a771bc74223b15a560c20368d294a495db9f
PallaviKhedle <pallavi8khedle@gmail.com>
2024-02-11 11:48:16 -0800

    ios changes

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,79 +1,182 @@
-import React, { useEffect } from "react";
-import { View, Text, Image, TouchableOpacity, Alert } from "react-native";
-import * as AuthSession from 'expo-auth-session';
-import * as Google from "expo-auth-session/providers/google";
-import { makeRedirectUri } from 'expo-auth-session';
+// import React, { useEffect, useState } from "react";
+// import { View, Text, Image, TouchableOpacity, Alert } from "react-native";
+// import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStorage
+// import * as AuthSession from 'expo-auth-session';
+// import * as Google from "expo-auth-session/providers/google";
+// import { makeRedirectUri } from 'expo-auth-session';
+// import * as WebBrowser from "expo-web-browser";
 
-import Colors from "../../style/colors";
+// import Colors from "../../style/colors";
 
-const LoginScreen = ({ navigation }) => {
+// WebBrowser.maybeCompleteAuthSession();
+// const LoginScreen = ({ navigation }) => {
 
-  useEffect(() => {
-    const handleAuthSession = async () => {
-      const redirectUrl = makeRedirectUri({ useProxy: true });
-      const androidClientId = "686074631278-cpml0o7kvti07gnds3cppu2gpa421e99.apps.googleusercontent.com";
-      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${androidClientId}&redirect_uri=${encodeURIComponent(redirectUrl)}&scope=https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/userinfo.email&access_type=offline&prompt=consent`;
-      
-      const { type, params } = await AuthSession.startAsync({ authUrl });
-      console.log('AuthUrl',authUrl, type)
+//   const [token, setToken] = useState("");
+//   const [userInfo, setUserInfo] = useState(null);
+
+//   const [request, response, promptAsync] = Google.useAuthRequest({
+//     androidClientId: "686074631278-cpml0o7kvti07gnds3cppu2gpa421e99.apps.googleusercontent.com",
+//     iosClientId: "686074631278-obhvesikr6juspq95v8bsbolvlh81ciq.apps.googleusercontent.com",
+//     webClientId: "686074631278-co8vng6rkbqrvh6juhtlm02klm4puqrj.apps.googleusercontent.com",
+//     // // Add redirectUri here
+//     redirectUri: makeRedirectUri({
+//     //   native: 'com.peace.mindPyramid',
+//       useProxy: true, // Add this line if you're not using standalone app, e.g., Expo Go
+//     }),
+//   });
+
+
+//   useEffect(() => {
+//     const signInWithGoogle = async () => {
+//       console.log("signInWithGoogle called, response:", response);
   
-      if (type === 'success') {
-        console.log("Authentication successful:", params);
-        // Handle successful authentication
-      } else {
-        console.log("Authentication canceled or failed");
-        // Handle other types of results (e.g., cancellation or failure)
-      }
-    };
+//       if (response?.type === "success") {
+//         console.log("Login Success, fetching user info...");
+//         await getUserInfo(response.authentication.accessToken);
+//       } else {
+//         console.log("Login not successful or waiting for login...");
+//         // Consider additional logic here for handling different response types (e.g., error, dismiss)
+//       }
+//     };
+  
+//     if (response) {
+//       signInWithGoogle();
+//     }
+//   }, [response]);
+  
+// console.log(JSON.stringify(userInfo))
 
-    handleAuthSession();
-  }, []);
 
-  const [, , promptAsync] = Google.useAuthRequest({
-    clientId: "686074631278-cpml0o7kvti07gnds3cppu2gpa421e99.apps.googleusercontent.com",
-    redirectUri: makeRedirectUri({ useProxy: true }),
-    scopes: ['profile', 'email'],
+//   const getLocalUser = async () => {
+//     const data = await AsyncStorage.getItem("@user");
+//     if (!data) return null;
+//     return JSON.parse(data);
+//   };
+
+//   const getUserInfo = async (token) => {
+//     if (!token) return;
+//     try {
+//       const response = await fetch(
+//         "https://www.googleapis.com/userinfo/v2/me",
+//         {
+//           headers: { Authorization: `Bearer ${token}` },
+//         }
+//       );
+// console.log('response',response)
+//       const user = await response.json();
+//       await AsyncStorage.setItem("@user", JSON.stringify(user));
+//       setUserInfo(user);
+//     } catch (error) {
+//       // Add your own error handler here
+//     }
+//   };
+
+  
+//   return (
+//     <View style={{ backgroundColor: Colors.white, flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Image
+//         source={require("../../assets/loginScreen.png")}
+//         style={{ width: 200, height: 200, marginBottom: 20 }}
+//       />
+//       <Text style={{ fontSize: 24, fontWeight: "bold", color: Colors.activeColor, marginBottom: 10 }}>
+//         Welcome to your Haven
+//       </Text>
+//       <Text style={{ fontSize: 16, color: Colors.lightBlue, marginBottom: 20 }}>
+//         Take a deep breath and begin your healing journey
+//       </Text>
+//       <TouchableOpacity
+//         style={{
+//           backgroundColor: Colors.lightBlue,
+//           borderRadius: 7,
+//           paddingHorizontal: 20,
+//           paddingVertical: 10,
+//           flexDirection: "row",
+//           alignItems: "center",
+//         }}
+//         onPress={() => {
+//             promptAsync();
+//           }}
+//       >
+//         <Image source={require("../../assets/gImage.png")} style={{ width: 30, height: 30, marginRight: 10 }} />
+//         <Text style={{ fontSize: 18, color: Colors.white }}>
+//           Login With Google
+//         </Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
+
+// export default LoginScreen;
+
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
+
+export default function LoginScreen() {
+  const [accessToken, setAccessToken] = React.useState(null);
+  const [user, setUser] = React.useState(null);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    androidClientId: "686074631278-cpml0o7kvti07gnds3cppu2gpa421e99.apps.googleusercontent.com",
+        iosClientId: "686074631278-obhvesikr6juspq95v8bsbolvlh81ciq.apps.googleusercontent.com",
+        clientId: "686074631278-co8vng6rkbqrvh6juhtlm02klm4puqrj.apps.googleusercontent.com",
   });
 
-  const handleLoginWithGoogle = async () => {
-    try {
-      await promptAsync();
-    } catch (error) {
-      console.error("Error:", error);
-      Alert.alert("Error", "Failed to authenticate with Google.");
+  React.useEffect(() => {
+    if(response?.type === "success") {
+      setAccessToken(response.authentication.accessToken);
+      accessToken && fetchUserInfo();
     }
-  };
-  
+  }, [response, accessToken])
+
+  async function fetchUserInfo() {
+    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    });
+    const useInfo = await response.json();
+    setUser(useInfo);
+  }
+
+  const ShowUserInfo = () => {
+    if(user) {
+      return(
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{fontSize: 35, fontWeight: 'bold', marginBottom: 20}}>Welcome</Text>
+          {/* <Image source={{uri: user.picture}} style={{width: 100, height: 100, borderRadius: 50}} /> */}
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user.name}</Text>
+        </View>
+      )
+    }
+  }  
+
   return (
-    <View style={{ backgroundColor: Colors.white, flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Image
-        source={require("../../assets/loginScreen.png")}
-        style={{ width: 200, height: 200, marginBottom: 20 }}
-      />
-      <Text style={{ fontSize: 24, fontWeight: "bold", color: Colors.activeColor, marginBottom: 10 }}>
-        Welcome to your Haven
-      </Text>
-      <Text style={{ fontSize: 16, color: Colors.lightBlue, marginBottom: 20 }}>
-        Take a deep breath and begin your healing journey
-      </Text>
-      <TouchableOpacity
-        style={{
-          backgroundColor: Colors.lightBlue,
-          borderRadius: 7,
-          paddingHorizontal: 20,
-          paddingVertical: 10,
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-        onPress={handleLoginWithGoogle}
-      >
-        <Image source={require("../../assets/gImage.png")} style={{ width: 30, height: 30, marginRight: 10 }} />
-        <Text style={{ fontSize: 18, color: Colors.white }}>
-          Login With Google
-        </Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      {user && <ShowUserInfo />}
+      {user === null &&
+          <>
+          <Text style={{fontSize: 35, fontWeight: 'bold'}}>Welcome</Text>
+          <Text style={{fontSize: 25, fontWeight: 'bold', marginBottom: 20, color: 'gray'}}>Please login</Text>
+        <TouchableOpacity
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+            }} 
+        >
+          <Image source={require("../../assets/gImage.png")} style={{width: 300, height: 40}} />
+        </TouchableOpacity>
+        </>
+      }
     </View>
   );
-};
+}
 
-export default LoginScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
