16be6c0346f6090759193d28bbad9ff04ae64e98
PallaviKhedle <pallavi8khedle@gmail.com>
2024-03-26 19:25:02 -0700

    added greeting card

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -13,16 +13,58 @@ import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { stories } from "./MeditationData";
 import PopularMP3 from "./PopularMP3";
+import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_KEY = "YOUR_YOUTUBE_API_KEY"; // Replace with your API key
+const API_KEY = "YOUR_YOUTUBE_API_KEY"; //AIzaSyB4KLNvjwSM8pifgko4XXbSs16zCOnT6hU
 
 export default function HomeScreen() {
   const popularMP3ImageWidth = Dimensions.get("window").width;
-
-  // Initialize state variables
+  
   const navigation = useNavigation();
+  // Initialize state variables
   const [videoList, setVideoList] = useState([]);
   const [selectedStory, setSelectedStory] = useState(null);
+  const [greeting, setGreeting] = useState("");
+
+  const [iconName, setIconName] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Function to determine time of day and construct greeting message
+    const getGreetingMessage = () => {
+      const now = new Date();
+      const hour = now.getHours();
+
+      if (hour >= 5 && hour < 12) {
+        setIconName("sun");
+        return "Good Morning";
+      } else if (hour >= 12 && hour < 17) {
+        setIconName("sunrise");
+        return "Good Afternoon";
+      } else if (hour >= 17 && hour < 21) {
+        setIconName("sunset");
+        return "Good Evening";
+      } else {
+        setIconName("moon");
+        return "Good Night";
+      }
+    };
+
+    const fetchUserFromStorage = async () => {
+      try {
+        const userData = await AsyncStorage.getItem("@user");
+        if (userData !== null) {
+          setUser(JSON.parse(userData));
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserFromStorage();
+    setGreeting(getGreetingMessage());
+  }, []);
 
   // Fetch data from YouTube API and set state
   useEffect(() => {
@@ -75,8 +117,13 @@ export default function HomeScreen() {
             </TouchableOpacity>
           ))}
         </ScrollView>
+        <View style={styles.greetingCard}>
+          <Feather name={iconName} size={30} color="#FFA500" />
+          <Text style={styles.greetingText}>{greeting}, {user ? user.name : "User"}!</Text>
+        </View>
         <PopularMP3 navigation={navigation} />
       </ScrollView>
+
     </LinearGradient>
   );
 }
@@ -88,6 +135,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  greetingCard: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    padding: 15,
+    borderRadius: 15,
+    alignItems: "center",
+    backgroundColor:'#CEDDE8'
+  },
+  greetingText: {
+    marginLeft:20,
+    fontSize: 18,
+    fontWeight: "bold",
+    color:'#FFA500'
   },
   storyContainer: {
     marginBottom: 15,
